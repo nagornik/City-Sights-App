5541e877bd92bb3abfef8a4a19e2aa113472e3fe
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-13 20:55:42 +0300

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Anton Nagornyi on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
